94a52b80c1bd0e37c4f146cdec2ac75a9c07bb4b
David Espinola <daver787@gmail.com>
2023-10-26 09:02:02 -0500

    Finished streamlit jumpstart.

--- 00_jumpstart/03_streamlit_jumpstart.py
+++ 00_jumpstart/03_streamlit_jumpstart.py
@@ -23,7 +23,7 @@ This dashboard provides insights into sales, customer demographics
 and product performance. Upload your data to get started. 
 """)
 # 2.0 Data Input
-st.header("upload Business Daata")
+st.header("Upload Business Data")
 
 uploaded_file = st.file_uploader("Choose a CSV file",
                                  type = "csv", accept_multiple_files = False)
@@ -31,20 +31,54 @@ uploaded_file = st.file_uploader("Choose a CSV file",
 # 3.0 App Body 
 #  What Happens Once Data Is Loaded?
 
-# * Sales insights
+# data = pd.read_csv("00_jumpstart/data/sales.csv")
+
+if uploaded_file:
+
+    data = pd.read_csv(uploaded_file)
+    st.write("Preview of Uploaded Data:")
+    st.write(data.head())
+
+    # * Sales insights
+    st.header("Sales Insights")
+    if 'sales_date' in data.columns and 'sales_amount' in data.columns:
+        st.write("Sales Over Time")
+        fig = px.line(data, x = 'sales_date', y = 'sales_amount', title = 'Sales Over Time')
+        st.plotly_chart(fig)
+
+    else:
+        st.warning("Please ensure your data has 'sales_date' and 'sales_amount' columns for sales visualization.")
 
 
-# * Customer Segmentation by Region
+    # * Customer Segmentation by Region
+    st.header("Customer Segmentation")
+    if 'region' in data.columns and 'sales_amount' in data.columns:
+        st.write("Customer Segmentation")
+        fig = px.pie(data, names = "region", values = "sales_amount")
+        st.plotly_chart(fig)
 
+    else:
+        st.warning("Please ensure your data has a 'region' column for customer segmentation")
+    # * Product Analysis
+    st.header("Product Analysis")
+    if 'product' in data.columns and 'sales_amount' in data.columns:
+        st.write("Top products by Sales")
+        top_products_df = data.groupby('product').sum('sales_amount').nlargest(10, 'sales_amount')
+        fig = px.bar(top_products_df, x = top_products_df.index, y = 'sales_amount', title = 'Top Products by Sales')
+        st.plotly_chart(fig)
 
-# * Product Analysis
+    else:
+        st.warning("Please ensure your data has 'product' and 'sales_amount' columns for product analysis")    
 
-
-# * Feedback Form
-
+    # * Feedback Form
+    st.header("Feedback (Your Opinion Counts)")
+    feedback = st.text_area("Please provide any feedback or suggestions")
+    if st.button("Submit Feedback"):
+        st.success("Thank you for your feedback")
 
 # 4.0 Footer
-
+st.write("---")
+st.write("This business dashboard is flexible. Expand upon it based on your specific business needs.")
 
 
 if __name__ == "__main__":
