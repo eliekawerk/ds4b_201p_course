1ab90ec79a4604c2ef8a4586281384acc60d68e5
David Espinola <daver787@gmail.com>
2023-10-27 08:40:15 -0500

    started work on main streamlit app.

--- 09_streamlit/01_app_streamlit.py
+++ 09_streamlit/01_app_streamlit.py
@@ -26,21 +26,51 @@ ENDPOINT = 'http://localhost:8000'
 
 # 1.0 TITLE
 
+st.title("Email Lead Scoring Streamlit FrontEnd Application")
 
 # 2.0 DATA INPUT
 #   CACHING DATA - NEEDED TO PREVENT REQUIRING DATA TO BE RE-INPUT
 
+uploaded_file = st.file_uploader(
+    "Upload Email Subscribers File",
+    type                  = ['csv'],
+    accept_multiple_files = False
+)
+
+@st.cache_data()
+def load_data(filename):
+    leads_df = pd.read_csv(filename)
+    return leads_df
+
 # 3.0 APP BODY
 
+if uploaded_file:
 
+    leads_df = load_data(uploaded_file)
+    full_data_json = leads_df.to_json()
     
     # Checkbox - Show Table 
+    if st.checkbox("Show Raw Data"):
+        st.subheader("Sample of Raw Data (First 10 Rows)")
+        st.write(leads_df.head(10))
     
-    
+
+    st.write("---")
+    st.markdown("# Lead Scoring Analysis")
     
     # User Inputs - Add Sliders / Buttons
+    estimated_monthly_sales = st.number_input(
+        "How much in email sales per month ($ on average)", 0, value = 250000, step =1000 )
+    monthly_sales_reduction_safe_guard = st.slider(
+        "How much of monthly sales should be mantained (%) ?", 0., 1., 0.9, step = 0.1
+    )
     
-    
+    #print(monthly_sales_reduction_safe_guard)
+
+    sales_limit = "$(:,.0f)".format
+    (monthly_sales_reduction_safe_guard * estimated_monthly_sales)
+
+    st.subheader(f"Monthly sales will not go below: {sales_limit}")
     
     
     # Run Analysis 
